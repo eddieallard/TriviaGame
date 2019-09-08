5fa5b8f333453e4364dcb07c85bf96766aa38a70
Eddie Allard <eddie@theallardcompany.com>
2019-09-08 13:21:26 -0400

    Variables and quiz questions updated

---
+++ assets/javascript/app.js
@@ -0,0 +1,39 @@
+// VARIABLES DECLARED
+var timer;
+var wins = 0;
+var losses = 0;
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+
+// QUESTIONS & ANSWERS FOR GAME
+
+var daQuiz = [
+    
+{
+    triviaQuestion: "In the movie Cast Away, what is the name of the Volleyball?",
+    choices: ["That Ball", "Wilson", "Alex", "Tom"],
+    answer: "Wilson"
+},
+{
+    triviaQuestion: "In the Sopranoes, what's the name of Tony's phychiaratrist?",
+    choices: ["Dr.FeelGood", "Dr.Sanjay Gupta", "Dr.Atkins", "Dr.Melfi"],
+    answer: "Dr.Melfi"
+},
+{
+    triviaQuestion: "In The Terminator, Sarah Conner was played by who?",
+    choices: ["Angella Bassett", "Rosie O'Donnel", "Linda Hamilton", "Cardi B"],
+    answer: "Linda Hamilton"
+},
+
+
+];
+
+
+// TIMER FUNCTION 
+
+
+// GAME LOGIC
+
+
+
+
